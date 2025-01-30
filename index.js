bd5c0bf10f9bdc3aefb4f2ffc57aacb99335ded5
idacarlin95 <carlin95@hotmail.se>
2025-01-30 17:14:29 +0100

    Adding sad.html and mood.html

--- index.js
+++ index.js
@@ -7,23 +7,55 @@ const PORT = 3001;
 http.createServer((req,res) => {
     res.writeHead(200, "Connection is successful", {"Content-type": "text/html"});
 
-    const fullpath = url.parse(req.url, true)
+    let fullpath = url.parse(req.url, true)
+    const mood = fullpath.query.mood;
+    let queries = fullpath.query;
 
     if (fullpath.pathname === "/") {
-        res.write("<h1>Add insttructions in an HTML file on how to operate this site! :)</div>")
-        res.end();
+        fs.readFile('./html/home.html', (err, data) => {
+            if (err) {
+                res.write("<h1>Error reading HTML file.</h1>");
+                res.end("Error");
+            } else {
+                res.write(data)
+            }
+            res.end();
+        })
+
     } else if (fullpath.pathname === "/mood") {
-        res.write("Hello there")
-        res.end();
-        console.log("Hi");
+        if (queries.mood === "sad") {
+            fs.readFile('./html/sad.html', (err, sadData) => {
+                if (err) {
+                    res.write('<h1>Error reading sad.html!</h1>');
+                    res.end("Error");
+                } else {
+                    res.end(sadData);
+                }
+            });
+
+        } else {
+            fs.readFile('./html/mood.html', (err, data) => {
+                if (err) {
+                    res.write("<h1>Error reading mood.html</h1>");
+                    res.end("Error");
+                } else {
+                    res.end(data);
+                }
+            });
+        }
+       
     } else if (fullpath.pathname === "/dark") {
         console.log("Hi");
     } else if (fullpath.pathname === "/light") {
         console.log("hi");
     } else if (fullpath.pathname === "/dogs") {
         console.log("hi");
-        res.write("Billie")
+        res.write("Bob")
         res.end();
     }
 
 }).listen(PORT, () => console.log(`Connecting on port ${PORT}`))
+
+    const readFile = async (fileName) => {
+        let data = await read(fileName);
+    }
